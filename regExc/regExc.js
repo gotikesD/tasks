2d1e8bcf61653e801ec8333f356b70701f39adbb
gotikesD <denis.derkach@chisw.com>
2016-02-25 17:40:30 +0200

    Deleting test file

--- regExc/regExc.js
+++ regExc/regExc.js
@@ -5,9 +5,9 @@
 //Для проверки html расскоментируйте вызов функции htmlTest();
 
     var email = new RegExp('^[a-z]{2,20}@{1}[a-z]{2,6}\.{1}[ru,com,ua]{1}');
-    var date = new RegExp('^[0-9]{2}[\. \-]\[a-z]{3,9}[\. \-][0-9]{4}');
+    var date = new RegExp('^[0-9]{2}[\. \-]\[a-z]{3,9}[\. \-][0-9]{4}$');
     var number = new RegExp('^\\(?\\+{1}380{1}\\)?[\- \\/]?[0-9]{2}[\- \\/]?[0-9]{3}[\- \\/]?[0-9]{2}[\- \\/]?[0-9]{2}$');
-    var html = new RegExp('^<{1}[a-z]{1,10}>{1}.{1,1000}<{1}\/{1}[a-z]{1,10}>{1}$');
+    var html = new RegExp('^<{1}[a-z]{1,10}>{1}[^</?a-z>].{1,1000}[^</?a-z>]<{1}\/{1}[a-z]{1,10}>{1}$');
 
 
 var dateTest = function() {
@@ -45,10 +45,12 @@ console.log('\n');
 
 var htmlTest = function() {
     var htmlArr = ['<div>Something...</div>' , '<p>Enother content ..</p>' , '<mark>123456789</mark>' ,
-                   '<div>You forget put / in -> <div>' , '<div>You forget write /div']
+                   '<div>Some text <p> <div>' , '<div></span> some text</div>']
     for(x =0; x < htmlArr.length; x++) {
         console.log(htmlArr[x] + ' - ' + html.test(htmlArr[x]));
     }
 }
 console.log('Html Test');
 htmlTest();
+
+
